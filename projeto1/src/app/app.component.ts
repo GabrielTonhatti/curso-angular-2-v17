f9ebcced3054c5548fb06c02891503f64fcdda8a
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2024-08-07 17:57:57 -0300

    8. Ciclo de vida de componentes Angular

--- projeto1/src/app/app.component.ts
+++ projeto1/src/app/app.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    OnDestroy,
+    AfterContentInit,
+    AfterViewInit,
+    AfterContentChecked,
+    AfterViewChecked
+{
   title = 'projeto1';
+
+  constructor() {
+    console.log('Componente construtor');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('Componente AfterContentChecked');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('Componente AfterViewChecked');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('Componente AfterViewInit');
+  }
+
+  ngOnDestroy(): void {
+    console.log('Componente OnDestroy');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('Componente AfterContentInit');
+  }
+
+  ngDoCheck(): void {
+    console.log('Componente DoCheck');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Componente OnChanges');
+  }
+
+  ngOnInit(): void {
+    console.log('Componente OnInit');
+    this.title = 'Novo título';
+  }
+
+  mudarTexto(): void {
+    this.title += 'a';
+  }
 }
